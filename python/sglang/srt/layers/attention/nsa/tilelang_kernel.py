1a7e35e4d306e66b60c2df313c3ac6d2f3c9885d
ning.qiao <ning.qiao@mthreads.com>
2026-02-05 13:34:19 +0800

    tilelang_kernel.py return to the version of the GitHub community.

--- python/sglang/srt/layers/attention/nsa/tilelang_kernel.py
+++ python/sglang/srt/layers/attention/nsa/tilelang_kernel.py
@@ -4,10 +4,9 @@ import tilelang
 import tilelang.language as T
 import torch
 
-tilelang.disable_cache()
-
 from sglang.srt.utils import is_hip, is_musa
 
+tilelang.disable_cache()
 tilelang.set_log_level("WARNING")
 
 pass_configs = {
@@ -22,9 +21,6 @@ FP32 = "float32"
 
 _is_hip = is_hip()
 _is_musa = is_musa()
-
-import logging
-logger = logging.getLogger(__name__)
 
 
 def fast_log2_ceil(x):
@@ -209,19 +205,18 @@ def fp8_index(
         tilelang.PassConfigKey.TL_DISABLE_WARP_SPECIALIZED: True,
     },
 )
-
 def sparse_attention_fwd_kernel_v1(
-    heads,
+    num_heads,
     dim,
     tail_dim,
     topk,
+    *,
     kv_group=1,
     sm_scale=None,
     is_causal=True,
     block_I=64,
     num_stages=2,
     threads=256,
-    indices_dtype="int32",
 ):
     assert dim == tilelang.math.next_power_of_2(
         dim
@@ -242,11 +237,12 @@ def sparse_attention_fwd_kernel_v1(
     seq_len = T.symbolic("seq_len")
     seq_len_kv = T.symbolic("seq_len_kv")
 
-    head_kv = heads // kv_group
-    q_shape = [batch, seq_len, heads, dim + tail_dim]
+    head_kv = num_heads // kv_group
+    q_shape = [batch, seq_len, num_heads, dim + tail_dim]
     kv_shape = [batch, seq_len_kv, kv_group, dim + tail_dim]
-    o_shape = [batch, seq_len, heads, dim]
+    o_shape = [batch, seq_len, num_heads, dim]
     indices_shape = [batch, seq_len, kv_group, topk]
+    indices_dtype = "int32"
     dtype = "bfloat16"
     accum_dtype = "float"
 
@@ -769,337 +765,26 @@ def sparse_attention_fwd_kernel_v2(
 
     return main
 
-@tilelang.jit(
-    out_idx=[-2, -1],
-    pass_configs={
-        tilelang.PassConfigKey.TL_DISABLE_TMA_LOWER: True,
-        tilelang.PassConfigKey.TL_DISABLE_WARP_SPECIALIZED: True,
-    },
-    verbose=True
-)
-def sparse_mla_fwd(
-    heads,
-    dim,
-    tail_dim,
-    topk,
-    kv_group=1,
-    sm_scale=None,
-    is_causal=True,
-    block_I=64,
-    num_stages=2,
-    threads=256,
-    indices_dtype="int32",
-):
-    assert dim == tilelang.math.next_power_of_2(
-        dim
-    ), f"haven't check padding correctness yet, dim={dim}"
-    assert tail_dim == tilelang.math.next_power_of_2(
-        tail_dim
-    ), f"haven't check padding correctness yet, dim={tail_dim}"
-    assert is_causal == True, "non-casual is not supported"
-    assert (
-        topk % block_I == 0
-    ), "otherwise will load some index=0 thus causing wrong kv to be loaded"
-    if sm_scale is None:
-        sm_scale = (1.0 / (dim + tail_dim)) ** 0.5 * 1.44269504  # log2(e)
-    else:
-        sm_scale = sm_scale * 1.44269504  # log2(e)
-
-    batch = T.dynamic("batch")
-    seq_len = T.dynamic("seq_len")
-    seq_len_kv = T.dynamic("seq_len_kv")
-
-    head_kv = heads // kv_group
-    q_shape = [batch, seq_len, heads, dim + tail_dim]
-    kv_shape = [batch, seq_len_kv, kv_group, dim + tail_dim]
-    o_shape = [batch, seq_len, heads, dim]
-    indices_shape = [batch, seq_len, kv_group, topk]
-    lse_shape = [batch, seq_len, heads]
-    dtype = "bfloat16"
-    accum_dtype = "float"
-
-    G = kv_group
-    H = head_kv
-    padded_H = max(tilelang.math.next_power_of_2(head_kv), 16)
-    if padded_H != H:
-        assert (
-            kv_group == 1
-        ), "here we solve the H padding automatically, other wise you should handle Q copy and Output copy with your mask (when kv_group == 1, use g_i * padded_H:(g_i+1) * padded_H would be handled automatically)"
-    BI = block_I
-    NI = tilelang.cdiv(topk, block_I)
-    D = dim
-    D_tail = tail_dim
-
-    if head_kv > 64:
-        assert head_kv % 64 == 0, "head_kv should be a multiple of 64"
-        REPLICATE_H = head_kv // 64
-    else:
-        REPLICATE_H = 1
-
-    H_per_block = padded_H if REPLICATE_H == 1 else 64
-
-    num_stages = 0
-    threads = 32
-
-    @T.prim_func
-    def main(
-        Q: T.Tensor(q_shape, dtype),  # type: ignore
-        KV: T.Tensor(kv_shape, dtype),  # type: ignore
-        Indices: T.Tensor(indices_shape, indices_dtype),  # type: ignore
-        Output: T.Tensor(o_shape, dtype),  # type: ignore
-        Lse: T.Tensor(lse_shape, accum_dtype),  # type: ignore
-    ):
-        with T.Kernel(seq_len * REPLICATE_H, batch, kv_group, threads=threads) as (
-            bx,
-            by,
-            bz,
-        ):
-            Q_shared = T.alloc_shared([H_per_block, D], dtype)
-            Q_tail_shared = T.alloc_shared([H_per_block, D_tail], dtype)
-            KV_shared_for_transpose = T.alloc_shared([BI, D], dtype)
-            KV_shared_for_no_transpose = T.alloc_shared([BI, D], dtype)
-            K_tail_shared = T.alloc_shared([BI, D_tail], dtype)
-            mask = T.alloc_fragment([BI], "bool")
-
-            acc_o = T.alloc_fragment([H_per_block, D], accum_dtype)
-            acc_s = T.alloc_fragment([H_per_block, BI], accum_dtype)
-            S_shared = T.alloc_shared([H_per_block, BI], dtype)
-            sumexp = T.alloc_fragment([H_per_block], accum_dtype)
-            sumexp_i = T.alloc_fragment([H_per_block], accum_dtype)
-            alpha = T.alloc_fragment([H_per_block], accum_dtype)
-            m_i = T.alloc_fragment([H_per_block], accum_dtype)
-            m_i_prev = T.alloc_fragment([H_per_block], accum_dtype)
-
-            T.fill(acc_o, 0)
-            T.fill(sumexp, 0)
-            T.fill(m_i, -(2**30))  # avoid -inf - inf to cause nan
-
-            b_i, g_i = by, bz
-            s_i = bx if REPLICATE_H == 1 else (bx // REPLICATE_H)
-            q_i = s_i
-            max_kv_i = q_i
-
-            H0 = g_i * padded_H + (0 if REPLICATE_H == 1 else (bx % REPLICATE_H) * 64)
-            H1 = H0 + H_per_block
-
-            T.copy(Q[b_i, s_i, H0:H1, :D], Q_shared)
-            T.copy(Q[b_i, s_i, H0:H1, D:], Q_tail_shared)
-
-            for i_i in T.Pipelined(NI, num_stages=num_stages):
-
-                for bi_i in T.Parallel(BI):
-                    mask[bi_i] = Indices[b_i, s_i, g_i, i_i * BI + bi_i] <= max_kv_i
-
-                for bi_i, d_i in T.Parallel(BI, D):
-                    kv_val = KV[b_i, Indices[b_i, s_i, g_i, i_i * BI + bi_i], g_i, d_i]
-                    KV_shared_for_transpose[bi_i, d_i] = kv_val
-                    KV_shared_for_no_transpose[bi_i, d_i] = kv_val
-                for bi_i, d_i in T.Parallel(BI, D_tail):
-                    K_tail_shared[bi_i, d_i] = KV[
-                        b_i, Indices[b_i, s_i, g_i, i_i * BI + bi_i], g_i, D + d_i
-                    ]
-
-                for h_i, bi_i in T.Parallel(H_per_block, BI):
-                    acc_s[h_i, bi_i] = T.if_then_else(
-                        mask[bi_i], 0, -T.infinity(acc_s.dtype)
-                    )
-                T.gemm(
-                    Q_shared,
-                    KV_shared_for_transpose,
-                    acc_s,
-                    transpose_B=True,
-                )
-                T.gemm(
-                    Q_tail_shared,
-                    K_tail_shared,
-                    acc_s,
-                    transpose_B=True,
-                )
-                T.copy(m_i, m_i_prev)
-                T.reduce_max(acc_s, m_i, dim=1, clear=False)
-                for h_i in T.Parallel(H_per_block):
-                    alpha[h_i] = T.exp2((m_i_prev[h_i] - m_i[h_i]) * sm_scale)
-                for h_i, bi_i in T.Parallel(H_per_block, BI):
-                    acc_s[h_i, bi_i] = T.exp2(
-                        acc_s[h_i, bi_i] * sm_scale - m_i[h_i] * sm_scale
-                    )
-                T.reduce_sum(acc_s, sumexp_i, dim=1)  # is this a accumulate operator?
-                for h_i in T.Parallel(H_per_block):
-                    sumexp[h_i] = sumexp[h_i] * alpha[h_i] + sumexp_i[h_i]
-                for h_i, d_i in T.Parallel(H_per_block, D):
-                    acc_o[h_i, d_i] = acc_o[h_i, d_i] * alpha[h_i]
-
-                T.copy(acc_s, S_shared)
-                T.gemm(S_shared, KV_shared_for_no_transpose, acc_o)
-
-            # Rescale
-            for h_i, d_i in T.Parallel(H_per_block, D):
-                acc_o[h_i, d_i] /= sumexp[h_i]
-            for h_i in T.Parallel(H_per_block):
-                sumexp[h_i] = T.log2(sumexp[h_i]) + m_i[h_i] * sm_scale
-
-            T.copy(acc_o, Output[b_i, s_i, H0:H1, :])
-            T.copy(sumexp, Lse[b_i, s_i, H0:H1])
-
-    return main
-
-def ref_sparse_mla_fwd_interface(q, kv, indices, sm_scale=None, is_casual=True):
-    q = q.float()
-    kv = kv.float()
-    # NOTE(MUSA): JIRA: https://jira.mthreads.com/browse/KUAE-840
-    # indices = indices.transpose(1, 2)
-    b, sq, h, dim_q = q.shape
-    b, sk, g, _ = kv.shape
-
-    assert kv.shape[-1] == 576, "you should assign dim otherwise"
-    dim = 512
-    k = kv
-    v = kv[..., :dim]
-
-    b, _, _, dim_v = v.shape
-    g_index = g
-    h_index = h // g
-    compressed_casual_mask = torch.arange(0, sq, dtype=torch.int32, device=q.device).view(-1, 1) >= torch.arange(
-        1 - 1, sk * 1, 1, dtype=torch.int32, device=q.device
-    ).view(1, -1)
-
-    # NOTE(MUSA): JIRA: https://jira.mthreads.com/browse/KUAE-840
-    # mask = q.new_zeros(b, g_index, sq, sk + 1, dtype=torch.bool).scatter(3, indices.long(), 1)
-    mask = q.new_zeros(b, sq, g_index, sk + 1, dtype=torch.bool).scatter(3, indices.long(), 1)
-    # NOTE(MUSA): JIRA: https://jira.mthreads.com/browse/KUAE-840
-    mask = mask.transpose(1, 2)
-    mask = mask[..., :-1]
-    mask = mask & compressed_casual_mask.view(1, 1, sq, sk)
-    mask[:, :, : 1 - 1, 0] = True
-    mask = mask.view(b, g_index, 1, sq, sk)
-
-    q = q.view(b, sq, g, -1, dim_q)
-
-    score = torch.einsum("bmghd,bngd->bghmn", q, k)
-    sm_scale = dim_q**-0.5 if sm_scale is None else sm_scale
-    score = score.masked_fill(~mask, float("-inf")).mul(sm_scale)
-    p = score.softmax(dim=-1)
-    assert not torch.isnan(p).any(), "NaN detected!"
-    p = p.view(b, g_index, h_index, -1, sq, sk)
-    p = p.view(b, g, -1, sq, sk)
-    o = torch.einsum("bghmn,bngd->bmghd", p.type(v.dtype), v)
-    o = o.reshape(b, sq, h, dim_v)
-    return o.to(torch.bfloat16)
 
 def tilelang_sparse_fwd(
     q: torch.Tensor,
     kv: torch.Tensor,
     indices: torch.Tensor,
     sm_scale: float,
-    return_p_sum: bool = False,
     d_v: int = 512,
-    block_I=64,
-    num_stages=2,
-    threads=256,
 ) -> torch.Tensor:
-    q = q.unsqueeze(0)
-    kv = kv.unsqueeze(0)
-    indices = indices.unsqueeze(0)
-
-    indices[indices < 0] = kv.shape[1]
-
-    USE_TORCH_IMPL = True
-    if USE_TORCH_IMPL:
-        ref_out = ref_sparse_mla_fwd_interface(q, kv, indices)
-        return ref_out
-
-    # Use tilelang impl
-    is_casual = True
-    assert return_p_sum == False, "This kernel file is for fwd only"
-    assert q.is_contiguous() and kv.is_contiguous() and indices.is_contiguous()
-    batch, seq_len, heads, dim_plus_tail_dim = q.shape
-    _, seq_len_kv, kv_group, _ = kv.shape
-
-    assert dim_plus_tail_dim == 576, "you should assign dim otherwise"
-    dim = d_v
-
-    assert kv.shape[-1] == dim_plus_tail_dim
-    tail_dim = dim_plus_tail_dim - dim
-    assert kv.shape[0] == batch
-    _, _, _, topk = indices.shape
-    assert indices.shape == (batch, seq_len, kv_group, topk)
-
-    indices_dtype = "int32"
-    if indices.dtype == torch.int64:
-        indices_dtype = "int64"
-
-    USE_MT_TILELANG_IMPL = False
-    if USE_MT_TILELANG_IMPL:
-        kernel = sparse_mla_fwd(
-            heads,
-            dim,
-            tail_dim,
-            topk,
-            kv_group,
-            sm_scale,
-            is_casual,
-            block_I=block_I,
-            num_stages=num_stages,
-            threads=threads,
-            indices_dtype=indices_dtype,
-        )
-        out, lse = kernel(q, kv, indices)
-    else:
+    assert q.dim() == 3 and kv.dim() == 3 and indices.dim() == 3
+    num_heads = q.shape[1]
+    dim = q.shape[2]
+    tail_dim = dim - d_v
+    topk = indices.shape[-1]
+    assert topk == 2048
+    if _is_hip or _is_musa:
         kernel = sparse_attention_fwd_kernel_v1(
-            heads,
-            dim,
-            tail_dim,
-            topk,
-            kv_group,
-            sm_scale,
-            is_casual,
-            block_I=block_I,
-            num_stages=num_stages,
-            threads=threads,
-            indices_dtype=indices_dtype,
+            num_heads, d_v, tail_dim, topk, sm_scale=sm_scale, num_stages=1
         )
-        out = kernel(q, kv, indices)
-
-    # kernel.show_source()
-    # out [B, S_q, H, 512], lse [B, S_q, H]
-    return out
-
-# def tilelang_sparse_fwd(
-#     q: torch.Tensor,
-#     kv: torch.Tensor,
-#     indices: torch.Tensor,
-#     sm_scale: float,
-#     d_v: int = 512,
-# ) -> torch.Tensor:
-#     assert q.dim() == 3 and kv.dim() == 3 and indices.dim() == 3
-#     num_heads = q.shape[1]
-#     dim = q.shape[2]
-#     tail_dim = dim - d_v
-#     topk = indices.shape[-1]
-#     assert topk == 2048
-#     if _is_hip:
-#         kernel = sparse_attention_fwd_kernel_v1(
-#             num_heads, d_v, tail_dim, topk, sm_scale=sm_scale, num_stages=1
-#         )
-#     elif _is_musa:
-#         # use torch impl
-#         # return ref_sparse_mla_fwd_interface(q.unsqueeze(0).cpu(), kv.unsqueeze(0).cpu(), indices.unsqueeze(0).cpu()).to(q.device)
-#         indices_dtype = "int32"
-#         if indices.dtype == torch.int64:
-#             indices_dtype = "int64"
-#         kernel = sparse_mla_fwd(
-#             num_heads,
-#             d_v,
-#             tail_dim,
-#             topk,
-#             sm_scale=sm_scale,
-#             num_stages=1,
-#             indices_dtype=indices_dtype,
-#         )
-#         out, lse = kernel(q.unsqueeze(0), kv.unsqueeze(0), indices.unsqueeze(0))
-#         return out
-#     else:
-#         kernel = sparse_attention_fwd_kernel_v2(
-#             num_heads, d_v, tail_dim, topk, sm_scale=sm_scale
-#         )
-#     return kernel(q.unsqueeze(0), kv.unsqueeze(0), indices.unsqueeze(0))  # type: ignore
+    else:
+        kernel = sparse_attention_fwd_kernel_v2(
+            num_heads, d_v, tail_dim, topk, sm_scale=sm_scale
+        )
+    return kernel(q.unsqueeze(0), kv.unsqueeze(0), indices.unsqueeze(0))  # type: ignore
