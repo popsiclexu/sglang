e35799367e98c16cc6e956f4ab2691b40a96681c
popsiclexu <zhenxuexu@gmail.com>
2026-05-06 10:29:20 +0800

    Support Piecewise Graph on MUSA

--- sgl-kernel/python/sgl_kernel/utils.py
+++ sgl-kernel/python/sgl_kernel/utils.py
@@ -56,7 +56,7 @@ def cache_once(fn):
 
 @cache_once
 def is_arch_support_pdl() -> bool:
-    if bool(torch.version.hip):
+    if bool(torch.version.hip) or bool(torch.version.musa):
         return False
     try:
         device = torch.cuda.current_device()
